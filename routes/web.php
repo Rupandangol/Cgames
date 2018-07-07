117b3a54dc72d7f7b99e970142525e37d9c4692c
b7Rexx <bij.maharjan@gmail.com>
2018-07-07 10:51:11 +0545

    check

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
+
 //main home page
 Route::get('/', function () {
     return view('Games/home');
 });
+
+
+//****************************************GAMES****************************************
+
+//Tic Tac Toe
+Route::get('games/TTT/index', 'TTTController@index');
